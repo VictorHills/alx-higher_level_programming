7de683b56cd69e3b686197851fd17072a5b07e88
pascal <georgepascal1010@gmail.com>
2022-11-28 23:57:00 +0000

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an intepreted, interactive, object_oriented programmming\language that combines remarkable powwer with very clear syntax"
+str = "Python is an intepreted, interactive, object_oriented programmming
+\language that combines remarkable powwer with very clear syntax"
 str = str[39:66] + str[106:112] + str[:6]
 print(str)
--- 0x00-python-hello_world/8_concat_edges.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-str = "Python is an intepreted, interactive, object_oriented programmming\language that combines remarkable powwer with very clear syntax"
-str = str[39:66] + str[106:112] + str[:6]
-print(str)
