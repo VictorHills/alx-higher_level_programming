cdc3d20476403a7ce9fe7bb1241b8354ea33589e
pascal <georgepascal1010@gmail.com>
2022-11-30 00:25:56 +0000

    12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Print the numbers from 1 to 100 seperated by a space.
+  For multile of three, print Fizz instead of the number
+  For multiles of five, print Buzz instead of the number
+  For mulltiples of three and five, print FizzBuz insteead of the number"""
+
+def fizbuzz():
+    for number in range(1, 100):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ", end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif nuber % 5 == 0:
+            print("Buzz", end="")
+        else:
+            print("{} ".format(number), end="")
