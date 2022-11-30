df5e191d97bad909778a791549da5d13b991cb40
pascal <georgepascal1010@gmail.com>
2022-11-30 01:27:25 +0000

    102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bn/python3
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided byb holberton school."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
