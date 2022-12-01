6b422002a45cfe7e2ff83b28e2ebabbb24c81e38
pascal <georgepascal1010@gmail.com>
2022-12-01 23:51:20 +0000

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name___ = "___main___":
+    """Print all names defined by hedden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
