7343032fda1d78f62e01f7f56104a558906f9d70
pascal <georgepascal1010@gmail.com>
2022-12-08 00:53:08 +0000

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -3,41 +3,42 @@
 
 /**
  * print_python_bytes - Prints bytes information
- * @p: python object
+ *
+ * @p: Python Object
  * Return: no return
  */
 void print_python_bytes(PyObject *p)
 {
-     char *string;
-     long int size, i, limit;
+	char *string;
+	long int size, i, limit;
 
-     printf("[.] bytes object info\n");
-     if (!PyBytes_Check(p))
-     {
-	printf(" [ERROR] Invalid Bytes Object\n");
-	return;
-     }
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
 
-     size = ((PyVarObject *)(p))->ob_size;
-     string = ((PyBytesObject *)->ob_sval;
+	size = ((PyVarObject *)(p))->ob_size;
+	string = ((PyBytesObject *)p)->ob_sval;
 
-     printf("  size: %ld\n", size);
-     printf("  trying string: %s\n", string);
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", string);
 
-     if (size >= 10)
-	 limit = 10;
-     else
-         limit = size + 1;
+	if (size >= 10)
+		limit = 10;
+	else
+		limit = size + 1;
 
-     printf("  first %ld bytes:", limit);
+	printf("  first %ld bytes:", limit);
 
-     for (i = 0; i < limit; i++)
-         if (string[i] >= 0)
-             printf(" %02x", string[i]);
-         else
-             printf(" %02x", 256 + string[i]);
+	for (i = 0; i < limit; i++)
+		if (string[i] >= 0)
+			printf(" %02x", string[i]);
+		else
+			printf(" %02x", 256 + string[i]);
 
-     printf("\n");
+	printf("\n");
 }
 
 /**
