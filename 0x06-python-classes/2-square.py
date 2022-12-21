75f0fee3c45e7be6f7f772ad5420dd94b2c019ad
pascal <georgepascal1010@gmail.com>
2022-12-21 11:27:52 +0000

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 
-
 """Define a class Square."""
 
 class Square:
+
     """Represents a square."""
 
     def __inti__(self, size=0):
-        """Initialize a new Square.
 
+        """Initialize a new Square.
         Args:
             size (init): The size of the mew Square.
-        raises:
+        Raises:
             TypeError: if size is not integer
             ValueError: if size is less than zero
         """
 
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         self.__size = size
