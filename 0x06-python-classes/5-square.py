05790a738a222b3903f9c1e7d9dcb7d3c9852a6d
Pascal George <41654419+AlPascal1010@users.noreply.github.com>
2022-12-21 12:46:48 +0100

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,38 +1,62 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
 
-"""Define a class Square"""
 
 class Square:
-    """Represent a square."""
-
-    def __init__(self, size):
-        """Initilizes a new square
-
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """initializes the square
         Args:
-            size (int): The size of the new square.
+            size (int): size of a side of the square
+        Returns:
+            None
         """
-         self.size = size
+        self.size = size
 
-     @property
-     def size(self):
-         """Get.set the currnet size of the square."""
-         return (self.__size)
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            The size of the square
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-           raise TypeError("size must be an integer")
-        elif value < 0:
-           raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+        """setter of __size
+        Args:
+            value (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
 
     def my_print(self):
-        """Print the square with the # charter."""
-        for i in range(0, self.__size):
+        """prints the square
+        Returns:
+            None
+        """
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("".join(["#" for j in range(self.__size)]))
             [print("#", end="") for j in range(self.__size)]
             print("")
         if self.__size == 0:
